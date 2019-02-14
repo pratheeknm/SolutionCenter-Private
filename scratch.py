d3bf90c7df4cb832fd3bf145cb1b40e7d7ca101e
kapils-repos <47316530+kapils-repos@users.noreply.github.com>
2019-02-14 11:44:48 +0530

    Add files via upload

--- scratch.py
+++ scratch.py
@@ -5,5 +5,6 @@ f=open("manifest.txt",'r',encoding = 'utf-8')
 a = f.read().split("\n")
 for x in a:
     if x.split(":")[0]=="Public":
-        if x.split(":")[1])=="YES":
+        print(x.split(":")[1])
+        if x.split(":")[1]=="YES":
             os.system("sh script.sh")
